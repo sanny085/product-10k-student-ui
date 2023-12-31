afc56a672b22fdc8a6caad90e985e830bb397c2f
sanny085 <sannykumar085@gmail.com>
2023-12-31 17:49:50 +0530

    Imported the homeScreen component in the main route

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,21 @@
 // import HomePage from "./(routes)/home/page.js";
+
 import {
   successStories,
   curriculum,
 } from "@/shared/staticData/homeScreen.json";
 import PageHeader from "@/widgets/PageHeader/PageHeader";
+import HomePageComponents from "@/components/HomeComponents/index";
 
 export default function Home() {
   return (
     <>
       {/* <HomePage /> */}
       <PageHeader />
+      <HomePageComponents
+        successStories={successStories}
+        curriculum={curriculum}
+      />
     </>
   );
 }
