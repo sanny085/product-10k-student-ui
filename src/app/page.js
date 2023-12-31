5dbc90907f306b28946441cec0ffaeb5f5fa8f4a
sanny085 <sannykumar085@gmail.com>
2023-12-31 18:11:55 +0530

    Changes the acctual file name

--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,14 @@ import {
   curriculum,
 } from "@/shared/staticData/homeScreen.json";
 import PageHeader from "@/widgets/PageHeader/PageHeader";
-import HomePageComponents from "@/components/HomeComponents/HomeComponents";
+import HomeComponents from "@/components/HomeComponents/HomeComponents";
 
 export default function Home() {
   return (
     <>
       {/* <HomePage /> */}
       <PageHeader />
-      <HomePageComponents
-        successStories={successStories}
-        curriculum={curriculum}
-      />
+      <HomeComponents successStories={successStories} curriculum={curriculum} />
     </>
   );
 }
