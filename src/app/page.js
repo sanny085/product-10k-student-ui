e1c03e44fd87d2204465f8bf15344bea7b8f1312
sanny085 <sannykumar085@gmail.com>
2023-12-31 19:15:34 +0530

    Created button

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import PageHeader from "@/widgets/PageHeader/PageHeader";
 // import Button from "@/components/Button";
 import Button from "@/widgets/Button";
 
-import HomeComponents from "@/components/HomeComponents/HomeComponents";
+import HomeComponents from "@/components/homeComponents/HomeComponents";
 
 export default function Home() {
   return (
